66a7b4d784bdb9858cc2bac60be9dc4fe59b238c
Bao Tran Duc <bao.tran@pyramid-consulting.com>
2017-02-28 17:13:29 +0700

    add jest test

---
+++ __tests__/components/CustomInputField.spec.ios.js
@@ -0,0 +1,19 @@
+// Link.react-test.js
+import React from 'react';
+import InputFieldComponent from 'src/components/CustomInputField.ios.js';
+import renderer from 'react-test-renderer';
+
+import constVar from 'src/const/constant';
+
+it('Test custom input field', (done) => {
+  const component = renderer.create(
+    <InputFieldComponent 
+        placeholder='User name'
+        imageIcon={require('assets/images/user_name.png')}
+        secureTextEntry={false} placeholderTextColor={constVar.colors.WHITE}
+        onChangeText={(userName) => this.setState({userName})}/>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  done();
+});
--- src/config_manager/store.js
+++ src/config_manager/store.js
@@ -1,7 +1,24 @@
-import { createStore, applyMiddleware } from 'redux';
+import { Platform } from 'react-native';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import devTools from 'remote-redux-devtools';
 
 import rootReducer from './rootReducer';
 
-export default store = createStore(rootReducer, applyMiddleware(thunk));
+let store;
+if(__DEV__){
+    const enhancer = compose(
+        applyMiddleware(thunk),
+        devTools({
+            name: Platform.OS,
+            hostname: 'localhost',
+            port: 5678
+        })
+    );
+    store = createStore(rootReducer, enhancer);
+} else {
+    store = createStore(rootReducer, applyMiddleware(thunk));
+}
+
+export default store;
 
